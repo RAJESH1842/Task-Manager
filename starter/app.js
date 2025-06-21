f7453c272041dc93d154624e475ca755f87e2369
Rajesh <rajeshsrrs2@gmail.com>
2025-06-22 03:25:46 +0530

    Initial clean commit

---
+++ starter/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express()
+const routes = require('./routes/tasks')
+
+
+app.use(express.json())
+
+app.get('/', (req, res)=>{
+    res.send('Task Manager App')
+})
+
+app.use('/api/v1/tasks', routes)
+
+const port = 3000;
+
+app.listen(port, console.log(`Server is listening on port ${port}...`))
